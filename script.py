058f86de0307c1cbdd61e6163f66ac254f6ef868
Venancio S L Cunha <venancio.slc@hotmail.com>
2023-12-13 04:50:52 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,13 @@
 import fdtd
-import fast_fdtd  
+import fdtd.backend as bd
+from fast_fdtd import FastFDTD 
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def run_pybind11_simulation(N, grid_size):
-    obj = fast_fdtd.FastFDTD()
+    obj = FastFDTD()
     obj.setGridSize(grid_size[0], grid_size[1],
                     np.zeros(grid_size), np.zeros(grid_size), np.zeros(grid_size),
                     np.zeros(grid_size), np.zeros(grid_size), np.zeros(grid_size),
@@ -19,21 +20,23 @@ def run_pybind11_simulation(N, grid_size):
 
 
 def run_native_fdtd_simulation(N, grid_size):
-    
-    sim = fdtd.Simulation(
-        size=grid_size,
-        resolution=1e-6,
-        CFL=0.99,
-        geometry=[fdtd.PointMaterial(), fdtd.PointMaterial()],
-        Courant=0.5
+    fdtd.set_backend("numpy")
+
+    WAVELENGTH = 1550e-9
+    SPEED_LIGHT: float = 299_792_458.0
+
+    grid = fdtd.Grid(
+        (2.5e-5, 1.5e-5, 1),
+        grid_spacing=0.1 * WAVELENGTH,
+        permittivity=1.0,
+        permeability=1.0,
     )
 
-    sim.add_source(fdtd.Gaussian1d(frequency=1e14, fwidth=1e13))
-
-    for _ in range(N):
-        sim.run(total_time=1e-14)
-
-    result = np.array(sim.electric_field[0].data)
+    grid[100, 60, 0] = fdtd.PointSource(
+        period=WAVELENGTH / SPEED_LIGHT, name="pointsource",
+    )
+    
+    result = grid.run(N, progress_bar=False)
 
     return result
 
@@ -52,8 +55,8 @@ def compare_simulations(N, grid_size):
     plt.show()
 
 if __name__ == "__main__":
-    N = 100  # Number of timesteps
-    grid_size = (300, 300)  # Size of the 2D simulation grid
+    N = 100  
+    grid_size = (300, 300)  
 
     compare_simulations(N, grid_size)
 
